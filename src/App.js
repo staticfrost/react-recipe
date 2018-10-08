4917e0c88a2b3fa6be409afac215fce5da00c43f
staticfrost <scott@scottbailey.net>
2018-10-08 11:36:23 +1000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = '1af908314527e129a0a5951522ab31c0';
+
+//https://www.food2fork.com/api/search?key=1af908314527e129a0a5951522ab31c0&q=chicken%20breast
 
 class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`
+    );
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+  };
+
+  componentDidMount = () => {
+    const json = localStorage.getItem('recipes');
+    const recipes = JSON.parse(json);
+
+    this.setState({ recipes: recipes });
+  };
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem('recipes', recipes);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
